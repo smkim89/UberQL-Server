c792987746dd44fca49e5b77495c2551a6090070
smkim89 <kiak357@gmail.com>
2018-08-06 23:49:26 +0900

    schema setup

---
+++ src/schema.ts
@@ -0,0 +1,6 @@
+import { GraphQLSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import path from "path";
+
+
