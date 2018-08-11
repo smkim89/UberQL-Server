2055472f724bd4f25511c8f101f8919b7c3bfa95
smkim89 <kiak357@gmail.com>
2018-08-11 14:05:24 +0900

    bcrypt를 이용한 암호화.

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,12 +1,18 @@
+import bcrypt, { hash } from "bcrypt";
 import { IsEmail } from "class-validator";
 import {
   BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
+
  @Entity()
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
@@ -41,10 +47,25 @@ class User extends BaseEntity {
   lastLat: number;
   @Column({ type: "float", default: 0 })
   lastOrientation: number;
-   
+  
+  //ascny를 사용했기때문에 Promise를 리턴해줌. type 이 promise고 리턴값이 없기에 void.
+
   get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+   private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
+
   @CreateDateColumn() createdAt: string;
   
   @UpdateDateColumn() updatedAt: string;
