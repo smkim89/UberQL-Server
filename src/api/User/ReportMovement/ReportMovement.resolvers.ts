2354ffe3ce3cd8052f1c647b3fce9536c7b3b66c
smkim89 <kiak357@gmail.com>
2018-08-16 21:00:30 +0900

    lat,lng ,orientation update User

---
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -0,0 +1,35 @@
+import User from "../../../entities/User";
+import {
+  ReportMovementMutationArgs,
+  ReportMovementResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { cleanNullArgs } from "../../../utils/cleanNullArgs";
+ const resolvers: Resolvers = {
+  Mutation: {
+    ReportMovement: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req }
+      ): Promise<ReportMovementResponse> => {
+        const user: User = req.user;
+        const notNullArgs = cleanNullArgs(args);
+        try {
+          await User.update({ id: user.id }, { ...notNullArgs });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
+  }
+};
+ export default resolvers;
