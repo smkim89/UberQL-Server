167f7f8fa05e98b1434b3d84f3d5013d4c3ae923
smkim89 <kiak357@gmail.com>
2018-08-18 14:35:09 +0900

    getMyPlaces 추가.

---
+++ src/api/Place/GetMyPlaces/GetMyPlaces.resolvers.ts
@@ -0,0 +1,39 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { GetMyPlacesResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+
+
+
+const resolvers : Resolvers = {
+    Query : {
+        GetMyPlaces : privateResolver( async ( _, __, { req } ) : Promise<GetMyPlacesResponse> => {
+            try{
+                const user = await User.findOne( { id: req.user.id }, { relations: ["places"] } );
+                if(user){
+                    return {
+                        ok : true,
+                        error : null,
+                        places : user.places
+                    }
+                }else{
+                    return {
+                        ok : false,
+                        error : "not found user.",
+                        places : null
+                    }
+                }
+            }catch(error){
+                return {
+                    ok : false,
+                    error : error.message,
+                    places : null
+                }
+            }
+            
+
+        } )
+    }
+}
+
+export default resolvers;
